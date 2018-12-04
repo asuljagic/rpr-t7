61c719c4aca29edf1e6d5a88cf7a44f668d51bff
asuljagic <43003728+asuljagic@users.noreply.github.com>
2018-12-04 19:47:51 +0100

    Uradjen 1. zadatak //TODO ucitajXml i zapisiXml

--- src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
@@ -11,6 +11,30 @@ import java.util.Scanner;
 
 public class Tutorijal {
 
+    public static ArrayList<Grad> ucitajGradove()throws FileNotFoundException {
+        ArrayList<Grad> gradovi = new ArrayList<Grad>();
+        Scanner ulaz;
+        try {
+            ulaz = new Scanner(new FileReader("mjerenja.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Datoteka mjerenja.txt ne postoji ili se ne može otvoriti");
+            throw e;
+        }
+        ulaz.useDelimiter("\n");
+        while (ulaz.hasNext()) {
+            String podaci = ulaz.next();
+            String[] nizPodataka = podaci.split(",");
+            String grad = nizPodataka[0];
+            Double[] temperature = new Double[nizPodataka.length-1];
+            for (int i = 1; i < nizPodataka.length; i++) {
+                temperature[i - 1] = Double.parseDouble(nizPodataka[i]);
+            }
+            gradovi.add(new Grad(grad, temperature));
+        }
+        ulaz.close();
+        return gradovi;
+    }
+
     public static void main(String[] args) {
     }
 }
