998802074d7f24226c7519b7b63378373d09fd16
asuljagic <43003728+asuljagic@users.noreply.github.com>
2018-12-04 21:24:02 +0100

    Uradjena sva 3 zadatka

--- src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
@@ -54,6 +54,46 @@ public class Tutorijal {
         return un;
     }
 
+    public static void zapisiXml(UN un){
+        try{
+            XMLEncoder izlaz = new XMLEncoder(new FileOutputStream("un.xml"));
+            izlaz.writeObject(un);
+            izlaz.close();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
     public static void main(String[] args) {
+        ArrayList<Grad> rez=new ArrayList<Grad>();
+
+        try {
+            rez = ucitajGradove();
+            for (var grad : rez) {
+                System.out.println(grad);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        Grad g1=rez.get(0);
+        Grad g2=rez.get(1);
+        Drzava drzava=new Drzava();
+        drzava.setGlavniGrad(g1);
+        drzava.setNaziv("Bih");
+        drzava.setBrStanovnika(45525);
+        drzava.setJedinicaZaPovrsinu("km");
+        drzava.setPovrsina(25525.0);
+
+        Drzava drzava1=new Drzava();
+        drzava1.setGlavniGrad(g2);
+        drzava1.setNaziv("BH");
+        drzava1.setBrStanovnika(455);
+        drzava1.setJedinicaZaPovrsinu("km");
+        drzava1.setPovrsina(225.0);
+
+        UN drz=new UN();
+        drz.dodajDrzavu(drzava);
+        drz.dodajDrzavu(drzava1);
+        zapisiXml(drz);
     }
 }
